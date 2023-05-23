d5cac2924e6896b25cff71e26494e44e61528541
Andrew <andrei.dmitrichenko@gmail.com>
2023-05-24 00:30:58 +0900

    cleaned up the code

--- backend/mancala/mancala-api/src/main/java/com/mygame/mancala/DTO/MancalaGameDto.java
+++ backend/mancala/mancala-api/src/main/java/com/mygame/mancala/DTO/MancalaGameDto.java
@@ -2,8 +2,5 @@ package com.mygame.mancala.DTO;
 
 import java.util.List;
 
-//public record MancalaGameDto(Long id, List<PlayerDto> players, GameStatusDto status, BoardDto board) {
-//}
-
 public record MancalaGameDto(Long id, List<PlayerDto> players, MancalaGameInfoDto info, BoardDto board) {
 }
--- backend/mancala/mancala-api/src/main/java/com/mygame/mancala/DTO/PlayerDto.java
+++ backend/mancala/mancala-api/src/main/java/com/mygame/mancala/DTO/PlayerDto.java
@@ -2,5 +2,14 @@ package com.mygame.mancala.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * Represents a data transfer object for a player.
+ *
+ * @param id                The ID of the player.
+ * @param addedToTheGame    Indicates whether the player has been added to the game.
+ *                          This field is annotated with @JsonIgnore to exclude it from serialization.
+ *                          It is needed temporarily to properly set the created player ID cookie,
+ *                          but should be removed once Spring Security is implemented.
+ */
 public record PlayerDto(Long id, boolean isHisTurn, @JsonIgnore boolean addedToTheGame) {}
 
